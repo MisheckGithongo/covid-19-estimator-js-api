ec5c6f0dd053bf05cc32688bc88b0898c6a5c2f7
Misheck Githongo <misheckgithongo@gmail.com>
2020-04-12 14:52:09 +0300

    fixed the return func

--- controllers/onCovid19Xml.js
+++ controllers/onCovid19Xml.js
@@ -22,31 +22,40 @@ exports.onCovid19Xml = (req, res) => {
       factor = Math.floor((days) / 3);
       sets = 2 ** factor;
     }
+    const signChecker = (num) => {
+      if (num > 0) {
+        return Math.floor(num);
+      }
+      if (num < 0) {
+        return Math.ceil(num);
+      }
+      return null;
+    };
     const returnData = {
       data: input,
       impact: {
         currentlyInfected: input.reportedCases * 10,
         infectionsByRequestedTime: input.reportedCases * 10 * sets,
         severeCasesByRequestedTime: Math.floor(0.15 * (input.reportedCases * 10 * sets)),
-        hospitalBedsByRequestedTime: (Math.floor(0.35 * input.totalHospitalBeds))
-        - (Math.floor(0.15 * (input.reportedCases * 10 * sets))),
+        hospitalBedsByRequestedTime: signChecker((0.35 * input.totalHospitalBeds)
+      - Math.floor(0.15 * (input.reportedCases * 10 * sets))),
         casesForICUByRequestedTime: Math.floor(0.05 * (input.reportedCases * 10 * sets)),
         casesForVentilatorsByRequestedTime: Math.floor(0.02 * (input.reportedCases * 10 * sets)),
-        // eslint-disable-next-line max-len
-        dollarsInFlight: ((input.reportedCases * 10 * sets) * (input.region.avgDailyIncomePopulation)
-         * (input.region.avgDailyIncomeInUSD) * days).toFixed(2)
+        dollarsInFlight: Math.floor((input.reportedCases * 10 * sets
+         * input.region.avgDailyIncomePopulation
+       * input.region.avgDailyIncomeInUSD) / days)
       },
       severeImpact: {
         currentlyInfected: input.reportedCases * 50,
         infectionsByRequestedTime: input.reportedCases * 50 * sets,
         severeCasesByRequestedTime: 0.15 * (input.reportedCases * 50 * sets),
-        hospitalBedsByRequestedTime: (Math.floor(0.35 * input.totalHospitalBeds))
-        - (Math.floor(0.15 * (input.reportedCases * 50 * sets))),
+        hospitalBedsByRequestedTime: signChecker((0.35 * input.totalHospitalBeds)
+      - Math.floor(0.15 * (input.reportedCases * 50 * sets))),
         casesForICUByRequestedTime: Math.floor(0.05 * (input.reportedCases * 50 * sets)),
         casesForVentilatorsByRequestedTime: Math.floor(0.02 * (input.reportedCases * 50 * sets)),
-        // eslint-disable-next-line max-len
-        dollarsInFlight: ((input.reportedCases * 50 * sets) * (input.region.avgDailyIncomePopulation)
-         * (input.region.avgDailyIncomeInUSD) * days).toFixed(2)
+        dollarsInFlight: Math.floor((input.reportedCases * 50 * sets
+        * input.region.avgDailyIncomePopulation
+       * input.region.avgDailyIncomeInUSD) / days)
       }
     };
     const xml = jsonxml(returnData);
